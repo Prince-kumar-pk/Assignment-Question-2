16e98d7233e94f59cedaf095c05d1b135db529f3
prince <princekumar92ssm@gmail.com>
2023-08-06 23:15:03 +0530

    testing

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 //Input
-/**
- * Strips the prefix from the keys of the given key-value pairs
- * @param {string} htmlContent - HTML content which needs to be highlighted 
- * @param {string} plainText - This plain text is extracted from htmlContent
- * @param {array} plainTextPositions - Array of Objects with start and end positions of words in plainText (Not the positions in HTML)
- * @returns {string} Using the positions in plainText, find the appropriate positions in htmlContent, highlight the content and return it
- */
+
 
 // sample taste case from the given sample example in problem statment
-const htmlContent = '<p><span>Hi David<br><br>Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects</span></p>';
-const plainText = 'Hi David Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects';
-const plainTextPositions = [
-    { start: 0, end: 8 },
-    { start: 19, end: 86 }
+let htmlContent = `<p><span>Hi David<br><br>Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects<br><br>Summary: Two deals with Morgan Stanley Renewables Inc. and Santander CIB will help finance the construction and operation of six utility Equity scale solar…<br><br>Read the full article <a href="https://content.seleritycorp.com/hosted/assets/www/UKMW47_hYz_RGzPSpHm44Hi1L49HdNBhs1OkKKW2OPI">here</a><br><br>-------------------------------------<br><br>You received this because you are subscribed to news related to <a href="https://iris.steeleye.co/market/instruments?search=ES0113900J37">ES0113900J37</a>, and this story was marked as 82% relevant.<br><br>Copyright of PR Newswire. All Rights Reserved. Terms and Conditions | Privacy Policy. To stop PR Newswire emails from getting removed by email filters please add our address (noreply@prnewswire.com) to your email address book. Registered Office: 3 Spring Mews, London SE11 5AN. Tel: +44 (0) 207 8405100. <br><br>To unsubscribe change your email preferences, please click <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">here</a>.<br><br>-------------------------------------<br><br><img src="https://context.seleritycorp.com/selerity/assets/sc_icons/pressRelease.png" alt="Rick Astley" style="width:100px;height:100px;"></span></p>`;
+let plainText = `Hi David Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects Summary: Two deals with Morgan Stanley Renewables Inc. and Santander CIB will help finance the construction and operation of six utility Equity scale solar… Read the full article here ------------------------------------- You received this because you are subscribed to news related to ES0113900J37 , and this story was marked as 82% relevant. Copyright of PR Newswire. All Rights Reserved. Terms and Conditions | Privacy Policy. To stop PR Newswire emails from getting removed by email filters please add our address (noreply@prnewswire.com) to your email address book. Registered Office: 3 Spring Mews, London SE11 5AN. Tel: +44 (0) 207 8405100. To unsubscribe change your email preferences, please click here . -------------------------------------`;
+let plainTextPositions = [
+    {
+        start: 241,
+        end: 247,
+    },
+    {
+        start: 518,
+        end: 525,
+    },
 ];
+
 
 function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
     // your logic goes here
@@ -30,10 +31,17 @@ function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
         var anshilight = `<mark>${portion}</mark>`;
 
         // Position of that portion in htmlContent
-        var startInd = htmlContent.indexOf(portion); // it return only starting position
+         var startInd = 0
+        
+        startInd = htmlContent.indexOf(portion); // it return only starting position
 
-        // console.log(startInd);
+        
+
+        console.log(startInd);
         //So for ending position of that portion 
+
+
+
         var endInd = startInd + portion.length;
 
         // console.log(endInd)
@@ -51,3 +59,6 @@ function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
   }
 
   console.log(highlightHTMLContent(htmlContent,plainText,plainTextPositions));
+
+
+  module.exports={highlightHTMLContent}
