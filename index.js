f9bf45eb2d85b9a9adb70e33eae276584d89748e
prince <princekumar92ssm@gmail.com>
2023-08-06 20:39:31 +0530

    completed

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-
-
+//Input
+/**
+ * Strips the prefix from the keys of the given key-value pairs
+ * @param {string} htmlContent - HTML content which needs to be highlighted 
+ * @param {string} plainText - This plain text is extracted from htmlContent
+ * @param {array} plainTextPositions - Array of Objects with start and end positions of words in plainText (Not the positions in HTML)
+ * @returns {string} Using the positions in plainText, find the appropriate positions in htmlContent, highlight the content and return it
+ */
 
 // sample taste case from the given sample example in problem statment
 const htmlContent = '<p><span>Hi David<br><br>Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects</span></p>';
